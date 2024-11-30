ac0868cbcbf4266ea2fc6ef908a6cfeb20a0a1ff
SpekledFrog <spekledfrog90@gmail.com>
2024-12-01 03:36:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 const youtubedl = require("youtube-dl-exec");
 const app = express();
 const port = 3000;
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
+// Enable CORS for all routes
+app.use(cors({
+  origin: '*', // Allow all origins
+  methods: 'GET,POST,OPTIONS,PUT,PATCH,DELETE',
+  allowedHeaders: 'X-Requested-With,content-type',
+  credentials: true
+}));
 
 app.get("/download-audio", (req, res) => {
   const url = req.query.url;
