30e32c73fc0a04c8c632c866f9d387d0caac5f6e
SpekledFrog <spekledfrog90@gmail.com>
2024-12-01 06:21:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = 3100;
 app.use(
   cors({
     origin:
-      "https://tesfgaydgsa-si2d.vercel.app/", // Your specific origin
+      "https://youtube2mp3-f399.vercel.app/", // Your specific origin
     methods: "GET,POST,OPTIONS,PUT,PATCH,DELETE",
     allowedHeaders: "X-Requested-With,content-type",
     credentials: true,
