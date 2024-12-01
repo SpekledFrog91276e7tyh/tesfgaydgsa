7bc53461b2e08e6c0f27c0f1417d1fae2539c2d4
SpekledFrog <spekledfrog90@gmail.com>
2024-12-01 15:50:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const youtubedl = require("youtube-dl-exec");
 const app = express();
-const port = 3100;
+const port = 3000;
 
 // Enable CORS for a specific origin
 app.use(
